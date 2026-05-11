da36787e80e6a687d040a07a79f969e4548c98bc
Sebilonchys <nugrasebas2@gmail.com>
2026-05-10 22:07:20 -0500

    feat(ui): add new product images and improve animations

--- js/script.js
+++ js/script.js
@@ -61,3 +61,62 @@ AOS.init({
     once: true,
 
 });
+
+
+// Abrir Card
+const viewButtons = document.querySelectorAll(".view-btn");
+const modal = document.querySelector(".image-modal");
+const modalImg = document.querySelector(".modal-img");
+const closeModal = document.querySelector(".close-modal");
+
+viewButtons.forEach(button => {
+
+    button.addEventListener("click", () => {
+
+        const img =
+            button.closest(".product-card")
+                .querySelector("img");
+
+        modalImg.src = img.src;
+
+        modal.classList.add("active");
+
+    });
+
+});
+
+closeModal.addEventListener("click", () => {
+    modal.classList.remove("active");
+});
+
+modal.addEventListener("click", (e) => {
+
+    if (e.target === modal) {
+        modal.classList.remove("active");
+    }
+
+});
+
+// ZOOM IMAGE 
+
+let zoomed = false;
+
+modalImg.addEventListener("click", () => {
+
+    if (!zoomed) {
+
+        modalImg.style.transform = "scale(1.8)";
+        modalImg.style.cursor = "zoom-out";
+
+        zoomed = true;
+
+    } else {
+
+        modalImg.style.transform = "scale(1)";
+        modalImg.style.cursor = "zoom-in";
+
+        zoomed = false;
+
+    }
+
+});
